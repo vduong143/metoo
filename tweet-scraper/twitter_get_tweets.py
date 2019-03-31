fa7a50004f55b94d5dd2861b912ba1ac298b8853
Viet Duong <vqduong14@gmail.com>
2019-03-31 19:05:47 -0400

    small fixes

--- tweet-scraper/twitter_get_tweets.py
+++ tweet-scraper/twitter_get_tweets.py
@@ -11,33 +11,35 @@ import progressbar
 def get_tweets(input_dir, output_dir, max_tweets, search_phrase, since, until):
     for file_name in sorted(os.listdir(input_dir)):
         if file_name.endswith('.json'):
-            start = time.time()
             print("Getting tweets from <{}>.".format(file_name))
             users = json.load(open(input_dir+'/'+file_name, 'r'))
-            tweet_batch = []
+            # tweet_batch = []
 
-            for user in users:
-                config = got.TweetCriteria()
-                config.setUsername(user["username"])
-                config.setSince(since)
-                config.setUntil(until)
-                config.setQuerySearch(search_phrase)
-                if max_tweets:
-                    config.setMaxTweets(max_tweets)
-                try:
-                    tweets = got.TweetManager.getTweets(config)
-                except:
-                    pass
-                if tweets:
-                    tweets = [tweet_to_dict(t) for t in tweets]
-                    for tweet in tweets:
-                        tweet["college"] = user["college"]
-                    tweet_batch.extend(tweets)
-            stop = time.time()
+            with progressbar.ProgressBar(max_value=len(users)) as bar:
+                for i in range(len(users)):
+                    config = got.TweetCriteria()
+                    config.setUsername(users[i]["username"])
+                    config.setSince(since)
+                    config.setUntil(until)
+                    config.setQuerySearch(search_phrase)
+                    if max_tweets:
+                        config.setMaxTweets(max_tweets)
+                    try:
+                        tweets = got.TweetManager.getTweets(config)
+                    except Exception as e:
+                        print(e)
+                        pass
+                    if tweets:
+                        tweets = [tweet_to_dict(t) for t in tweets]
+                        for tweet in tweets:
+                            tweet["college"] = users[i]["college"]
+                        # tweet_batch.extend(tweets)
+                        with open(output_dir+'/'+file_name, 'a') as tweet_file:
+                            json.dump(tweets, tweet_file, indent=2)
+                    bar.update(i)
+
             print("{} done".format(file_name))
-            with open(output_dir+'/'+file_name, 'w') as tweet_file:
-                json.dump(tweet_batch, tweet_file, indent=2)
-            print("Writing {}> done.".format(file_name))
+            # print("Writing {}> done.".format(file_name))
             print("Sleeping to avoid rate limit.")
             for i in progressbar.progressbar(range(60*15)):
                 time.sleep(1)
