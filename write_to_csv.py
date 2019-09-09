891bee30226517ae138fdb6a1a66d9d9758243ab
Viet Duong <vqduong14@gmail.com>
2019-09-09 02:10:26 -0400

    cleaned up tweet scraper

--- write_to_csv.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-txt_file = r"/home/vietduong/Desktop/metoo_project/metoo_tweets.txt"
-csv_file = r"/home/vietduong/Desktop/metoo_project/metoo_tweets.csv"
-
-in_txt = csv.reader(open(txt_file,"rb"),delimiter = "\t")
-out_csv = csv.writer(open(csv_file,"wb"))
-out_csv.writerows(in_txt)
